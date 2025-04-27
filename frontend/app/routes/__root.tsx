32f30bbc3702d1c24aea3f677a288ea855f5d80e
Josh <jdpuglielli@gmail.com>
2025-04-26 19:48:31 -0700

    Added TS query

--- frontend/app/routes/__root.tsx
+++ frontend/app/routes/__root.tsx
@@ -1,45 +1,50 @@
-// This is the entry point for all other routes. 
+// This is the entry point for all other routes.
 // The code in this file will wrap all other routes in the application.
 
-// Other than the client entry point, the __root route of your application is the entry point for your application. 
-// The code in this file will wrap all other routes in the app, including your home page. 
+// Other than the client entry point, the __root route of your application is the entry point for your application.
+// The code in this file will wrap all other routes in the app, including your home page.
 // It behaves like a pathless layout route for your whole application.
 
 // Because it is always rendered, it is the perfect place to construct your application shell and take care of any global logic.
 
 // app/routes/__root.tsx
-import type { ReactNode } from 'react'
+import type { ReactNode } from "react";
 import {
   Outlet,
   createRootRoute,
   HeadContent,
   Scripts,
-} from '@tanstack/react-router'
+} from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
   head: () => ({
     meta: [
       {
-        charSet: 'utf-8',
+        charSet: "utf-8",
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
       },
       {
-        title: 'TanStack Start Starter',
+        title: "TanStack Start Starter",
       },
     ],
   }),
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <RootDocument>
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </RootDocument>
-  )
+  );
 }
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
@@ -53,5 +58,5 @@ function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
         <Scripts />
       </body>
     </html>
-  )
+  );
 }
