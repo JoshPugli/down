aaf40966610d901c0b13093e541e7cea9fd14f6a
Josh <jdpuglielli@gmail.com>
2025-04-29 11:38:02 -0700

    Added drizzle connector

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,5 +4,5 @@
 import { signIn } from "next-auth/react"
  
 export default function SignIn() {
-  return <button onClick={() => signIn("google")}>Sign in with Google</button>
+  return <button onClick={() => signIn("google", { redirectTo: "/dashboard" })}>Sign in with Google</button>
 }
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,6 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { usersTable } from "./schema";
 
-// const db = drizzle(process.env.DATABASE_URL!);
-const db = drizzle(process.env.DATABASE_URL!, {
-  schema: { usersTable },
-});
+const db = drizzle(process.env.DATABASE_URL!);
 
 export default db;
